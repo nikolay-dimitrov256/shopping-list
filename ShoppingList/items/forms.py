ea1fe13658d783c0063f751bb22f7e7360ddee6d
nikolay-dimitrov256 <raystlin7@gmail.com>
2026-09-23 17:07:57 +0300

    Made a RangeInput to change the appearance of boolean fields

--- ShoppingList/items/forms.py
+++ ShoppingList/items/forms.py
@@ -1,9 +1,20 @@
 from django import forms
 
+from ShoppingList.common.forms import RangeInput
 from ShoppingList.items.models import Item
 
 
 class ItemBaseForm(forms.ModelForm):
+    is_urgent = forms.BooleanField(
+        label='Спешно',
+        widget=RangeInput(
+            attrs={
+                'min': 0,
+                'max': 1,
+                'step': 1,
+            }
+        )
+    )
     class Meta:
         model = Item
         fields = ['name', 'notes', 'quantity', 'unit', 'is_urgent', 'store', 'category']
